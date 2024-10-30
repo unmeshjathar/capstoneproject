d6758d35329b35057cabd96b5356ec99983c903b
unmeshjathar <73338366+unmeshjathar@users.noreply.github.com>
2024-10-30 23:50:27 +0530

    Add files via upload

---
+++ frontend-developer-capstone-main/frontend-developer-capstone-main/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
